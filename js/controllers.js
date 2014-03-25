34639b1911293860e5f91dffeaf6cf01c1c42e73
kevin <iscarryon@gmail.com>
2014-03-25 23:06:42 +0800

    initial commit

---
+++ js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('starter.controllers', [])
+
+
+// A simple controller that fetches a list of data from a service
+.controller('PetIndexCtrl', function($scope, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  // $scope.pets = PetService.all();
+
+  // run map initialize function when load complete
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  //map initialize function
+  function initialize(){
+  	var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+  }
+
+  function drawMap(latlng) {
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        debugger
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Greetings!"
+        });
+    }
+
+
+})
+
+
+// A simple controller that shows a tapped item's data
+.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pet = PetService.get($stateParams.petId);
+});
